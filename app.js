4e0e5cc7ec64818be0540f64b39dad91d0ea75b6
shahriar jalal <shahjalalsbc@gmail.com>
2021-02-11 12:48:17 +0600

    js classList property

---
+++ app.js
@@ -0,0 +1,6 @@
+const userId =document.getElementById("profile-id")
+
+const doCss = () => {
+    //add multiple class to profile-d
+    userId.classList.add("classOne", "classTwo", "classThird")
+}
