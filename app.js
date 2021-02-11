28e75eaa16658239037110eba310bd095ff19d15
shahriar jalal <shahjalalsbc@gmail.com>
2021-02-11 13:21:52 +0600

    JS classList property

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const userId =document.getElementById("profile-id")
+const userId = document.getElementById("profile-id");
+const centeredId = document.getElementById("position");
+const toggleId = document.getElementById("toggle-id");
 
 const doCss = () => {
     //add multiple class to profile-d
-    userId.classList.add("classOne", "classTwo", "classThird")
+    userId.classList.add("classOne", "classTwo", "classThird");
+    centeredId.classList.remove("center");
+    centeredId.innerText = "all styled removed";
+    centeredId.innerHTML = `
+        <h2>JS is awesome</h2>
+        <h5>Let make something creative</h5>
+    `
+
+    toggleId.classList.toggle("toggleClass")
 }
